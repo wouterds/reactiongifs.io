9966a063f0cd96c15a64b299d664c4f4452f0958
WouterDS <wouter.de.schuyter@gmail.com>
2015-03-23 00:30:31 +0100

    Beta access flag

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -7,6 +7,12 @@ class EntryController extends Controller {
 
 	public function index()
 	{
+		$access = Request::input('beta');
+
+		if (!$access) {
+			die('Work in progress, check back later');
+		}
+		
 		$entries = Entry::with('picture')->orderBy('published_at', 'ASC')->paginate(3);
 
 		return view('entry/index')->with('entries', $entries);
